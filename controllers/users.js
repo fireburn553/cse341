9c3b1b41662059e8a07903e294af623a67becc98
James Charlie I. Salva <56073573+fireburn553@users.noreply.github.com>
2024-04-27 08:23:51 +0800

    Added rest file and fix error in getSingle function

--- controllers/users.js
+++ controllers/users.js
@@ -14,7 +14,7 @@ const getSingle = async (req, res) => {
     const result = await mongodb.getDatabase().db().collection('users').find({_id: userId});
     result.toArray().then((users) => {
       res.setHeader('Content-Type', 'application/json');
-      res.status(200).json([0])
+      res.status(200).json(users[0])
     });
 };
 
