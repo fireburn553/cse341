ae5f02da8ad7e0cb25b84494888a9413d91abde5
James Charlie I. Salva <56073573+fireburn553@users.noreply.github.com>
2024-05-03 21:43:11 +0800

    Added other endpoints

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,9 +1,15 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const userController = require('../controllers/users');
+const userController = require("../controllers/users");
 
-router.get('/', userController.getAll);
+router.get("/", userController.getAll);
 
-router.get('/:id', userController.getSingle);
+router.get("/:id", userController.getSingle);
+
+router.post("/", userController.createContact);
+
+router.put("/:id", userController.updateContact);
+
+router.delete("/:id", userController.deleteContact);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const express = require('express');
-
-const mongodb = require('./data/database');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongodb = require("./data/database");
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use('/', require('./routes'));
+app.use(bodyParser.json());
+app.use("/", require("./routes"));
 
-
-mongodb.initDb((err)=> {
-    if(err){
-        console.log(err);
-    }
-    else{
-        app.listen(port, () => {console.log(`Running on port ${port}`)})
-    }
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port, () => {
+      console.log(`Running on port ${port}`);
+    });
+  }
 });
-
